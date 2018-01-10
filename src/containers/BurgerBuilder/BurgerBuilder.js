e672c8e9d4bfc00db5ed9ca5a6d83520a493fc43
Travis Shields <travis.shields@gmail.com>
2018-01-09 19:36:47 -0500

    Set up firebase and send post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -61,7 +62,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Continue');
+    const order = {
+      ingredients : this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Travis Shields',
+        address: {
+          street: '100 Jay Street',
+          zipCode: '11201',
+          country: 'USA'
+        },
+        email: 'travis.shields@gmail.com'
+      },
+      delieveryMethod: 'fastest'
+    };
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render () {
